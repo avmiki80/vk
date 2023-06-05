e9a06093a57d20435fa9b6945c3e07988b01718f
avm <avmiki80@mail.ru>
2023-06-05 21:01:05 +0500

    тестовое задание. Вроде все.

--- src/main/java/ru/avm/vktest/service/VkQueue.java
+++ src/main/java/ru/avm/vktest/service/VkQueue.java
@@ -1,16 +1,8 @@
 package ru.avm.vktest.service;
 
 import com.vk.api.sdk.client.AbstractQueryBuilder;
-import com.vk.api.sdk.objects.Validable;
-import com.vk.api.sdk.objects.friends.responses.GetResponse;
-import com.vk.api.sdk.objects.groups.responses.GetObjectExtendedResponse;
 import lombok.Getter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import ru.avm.vktest.dto.VkData;
-
-import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
